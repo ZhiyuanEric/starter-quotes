0fe1c285ae146ee5a757e4f12d0644dbb6df5d2d
ZhiyuanEric <linzhiyuaneric@yahoo.com>
2018-01-21 14:13:15 -0800

    step 8
    edit format

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -1,9 +1,10 @@
 <div class="row">
     {authors}
-    <div class="span4">
+    <div class="span4" style="text-align: center">
         <a href="/welcome/show/{key}">
             <img src="/pix/{who}.jpg" title="{who} plays {role}"/>
         </a>
+		<p>{who}</p>
     </div>
     {/authors}
 </div>
